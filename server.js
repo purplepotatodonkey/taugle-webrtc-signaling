909db5066424d9c6e1eb649905d2856d130ad6a7
Bobby Dim <bobbydim@Bobbys-MBP-3.carolina.rr.com>
2024-11-07 20:16:50 -0500

    add ice-candidate/offer/answer message

--- server.js
+++ server.js
@@ -16,9 +16,19 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   socket.emit('New client connected', socket.id);
 
-  socket.on('message', (message) => {
-    socket.broadcast.emit('message', message);
-    console.log("message", message)
+  socket.on('offer', (offer) => {
+    socket.broadcast.emit('offer server', offer);
+    console.log("offer server", offer)
+  });
+
+  socket.on('answer', (answer) => {
+    socket.broadcast.emit('answer server', answer);
+    console.log("answer server", answer)
+  });
+
+  socket.on('ice-candidate', (candidate) => {
+    socket.broadcast.emit('ice-candidate server', candidate);
+    console.log("ice-candidate server", candidate)
   });
 
   socket.on('disconnect', () => {
